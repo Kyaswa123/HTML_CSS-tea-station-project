d67d5f5a565d7f9c170d494a5eb147e2a5957b6c
Kyaswa123 <kurubayaswanth878@gmail.com>
2023-01-28 21:42:30 +0530

    final_projects

--- HTML_CSS-Tea-station-projects/app.js
+++ HTML_CSS-Tea-station-projects/app.js
@@ -1,6 +1,7 @@
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
-const navclose = document.getElementById("navclose");
+const navClose = document.getElementById("nav-close");
 
-navBtn.addEventListener("click", () => {navbar.classList.add("showNav")});
-navclose.addEventListener("click", () => {navbar.classList.add("showNav")});
+navBtn.addEventListener("click", () => { navbar.classList.add("showNav") });
+
+navClose.addEventListener("click", () => { navbar.classList.remove("showNav") });
